1e30bd94fd937952389e2ce6041f1684a1414d0e
Kristoffer LR <kristoffer.lr@gmail.com>
2013-07-14 18:29:52 -0400

    The filter was no more effective in the checkout after using the "Calculate" shipping button.

--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link:
 Tags: 
 Requires at least: 3.5
 Tested up to: 3.5.1
-Stable tag: 1.0
+Stable tag: 1.1
 License: 
 License URI: 
 
@@ -12,10 +12,16 @@ Filter for "wp-e-commerce" shipping options.
 
 == Description ==
 
-Filter the cart of **wp-e-commerce** to add/remove the *fixed_rate_shipping_option* of the **wp-e-commerce-fixed-rate-shipping** plugin based on user's *fixed_rate_shipping_option* capability.
+Filter the checkout shipping options of **wp-e-commerce** to remove the *simple_shipping* of the **wp-e-commerce-fixed-rate-shipping** plugin based on user's *fixed_rate_shipping_option* capability.
 
 == Installation ==
 
 1. Upload `wp-e-commerce-filter-shipphing-options` to the `/wp-content/plugins/` directory
 2. Activate the plugin through the 'Plugins' menu in WordPress
 3. Create a capability (permission) named "fixed_rate_shipping_option" through your favorite *user roles manager* plugin
+
+== Changelogs ==
+
+=== 1.1 ===
+
+* The filter was no more effective in the checkout after using the "Calculate" shipping button.
--- wp-e-commerce-filter-shipphing-options.php
+++ wp-e-commerce-filter-shipphing-options.php
@@ -3,49 +3,63 @@
 Plugin Name: WP e-Commerce - Filter Shipping Options
 Plugin URI: 
 Description: Filter for "wp-e-commerce" shipping options.
-Version: 1.0
+Version: 1.1
 Author: KLicheR
 Author URI: http://profiles.wordpress.org/klicher
 License: 
 */
 
-/**
- * Filter the cart of "wp-e-commerce" to add/remove the
- * "fixed_rate_shipping_option" of the "wp-e-commerce-fixed-rate-shipping"
- * plugin based on user's *fixed_rate_shipping_option* capability.
- */
-function wp_e_commerce_filter_shipphing_options_remove_shipping_option_based_on_roles($profile) {
-	if (array_key_exists('cart', $profile)) {
-		$cart = maybe_unserialize($profile['cart']);
-		if (
-			is_object($cart)
-			&& !is_wp_error($cart)
-			&& property_exists($cart, 'shipping_methods')
-			&& is_array($cart->shipping_methods)
-		) {
-			$key = array_search('simple_shipping', $cart->shipping_methods);
+// Determine if a user currently logged in and have the capability "fixed_rate_shipping_option".
+$wp_e_commerce_filter_shipphing_options_user_login_has_capability = false;
 
-			// If the user have access to "fixed_rate_shipping_option"...
-			if (current_user_can('fixed_rate_shipping_option')) {
-				// ...and doesn't have it...
-				if (false === $key) {
-					// ...replace the methods by what's set in the admin.
-					$cart->shipping_methods = get_option('custom_shipping_options', array());
-					$profile['cart'] = maybe_serialize($cart);
-				}
-			}
-			// If the user doesn't have access to "fixed_rate_shipping_option"...
-			else{
-				// ...and have it...
-				if (false !== $key) {
-					// ...remove it.
-					array_splice($cart->shipping_methods, $key, 1);
-					$profile['cart'] = maybe_serialize($cart);
-				}
-			}
-		}
+/**
+ * Filter the option "custom_shipping_options" and remove the shipping option
+ * "simple_shipping" if the current user or the user that try to logged in
+ * doesn't have the capability "fixed_rate_shipping_option".
+ */
+function wp_e_commerce_filter_shipphing_options_filter_option_custom_shipping_options($option) {
+	global $wp_e_commerce_filter_shipphing_options_user_login_has_capability;
+
+	$key = array_search('simple_shipping', $option);
+	// If the shipping option "simple_shipping" exists...
+	// ...and the current user doesn't have the capability "fixed_rate_shipping_option"...
+	// ...and no user are currently trying to logged in and have the capability "fixed_rate_shipping_option".
+	if (
+		false !== $key
+		&& !current_user_can('fixed_rate_shipping_option')
+		&& false === $wp_e_commerce_filter_shipphing_options_user_login_has_capability
+	) {
+		// ...remove it.
+		array_splice($option, $key, 1);
 	}
 
-	return $profile;
+	return $option;
 }
-add_filter('wpsc_get_all_customer_meta', 'wp_e_commerce_filter_shipphing_options_remove_shipping_option_based_on_roles');
+add_filter('option_custom_shipping_options', 'wp_e_commerce_filter_shipphing_options_filter_option_custom_shipping_options');
+
+/**
+ * Update the shipping methods of the current cart when a user logged in and
+ * have the capability "fixed_rate_shipping_option".
+ */
+function wp_e_commerce_filter_shipphing_options_login($user_login, $user) {
+	global $wpsc_cart, $wp_e_commerce_filter_shipphing_options_user_login_has_capability;
+
+	// If the user that logged in have the capability "fixed_rate_shipping_option".
+	if (
+		is_object($user)
+		&& property_exists($user, 'allcaps')
+		&& is_array($user->allcaps)
+		&& isset($user->allcaps['fixed_rate_shipping_option'])
+		&& $user->allcaps['fixed_rate_shipping_option'] === true
+	) {
+		// Let the "wp_e_commerce_filter_shipphing_options_filter_option_custom_shipping_options"
+		// filter do his job cause the "current_user_can" function doesn't work
+		// at this point.
+		$wp_e_commerce_filter_shipphing_options_user_login_has_capability = true;
+		// Update the shipping methods.
+		$wpsc_cart->get_shipping_method();
+
+		$wp_e_commerce_filter_shipphing_options_user_login_has_capability = false;
+	}
+}
+add_action('wp_login', 'wp_e_commerce_filter_shipphing_options_login', 10, 2);
